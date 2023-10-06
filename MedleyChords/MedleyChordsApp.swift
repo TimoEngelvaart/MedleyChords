7a22b69e04aceeedaf0ce8e54af0738f3eb00e68
Timo <timoengelvaart@live.nl>
2023-10-06 12:13:46 +0200

    Initial Commit

---
+++ MedleyChords/MedleyChordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedleyChordsApp.swift
+//  MedleyChords
+//
+//  Created by Timo Engelvaart on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MedleyChordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
